9c245afecacec3721c79d8ff470a85da02209c0f
Administrator <Administrator@192.168.1.147>
2016-04-21 15:20:51 +0800

    2016.4.21 finished part7test

---
+++ TestJUnit/src/part7test.java
@@ -0,0 +1,257 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class part7test {
+
+	//@Test
+	public void list_hotel_photos() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("hotel_id", "cnbjbjcgmz"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/list_hotel_photos");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void list_room_type_photos() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("room_type_id", "649"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/list_room_type_photos");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+
+	//@Test
+	public void list_hotel_albums() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("hotel_id", "cnbjbjcgmz"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/list_hotel_albums");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void list_album_photos() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("album_id", "25"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/list_album_photos");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void list_news() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("news_type_name", ""));
+		formparams.add(new BasicNameValuePair("only_mobile", ""));
+		formparams.add(new BasicNameValuePair("no_content", "true"));
+		formparams.add(new BasicNameValuePair("page", ""));
+		formparams.add(new BasicNameValuePair("page_size", ""));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/list_news");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void get_news_detail() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("news_id", "7287"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/get_news_detail");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void search_questions() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("condition.page", ""));
+		formparams.add(new BasicNameValuePair("condition.page_size", ""));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/search_questions");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void add_question() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("question.asker_name", "Johnny"));
+		formparams.add(new BasicNameValuePair("question.asker_mobile", ""));
+		formparams.add(new BasicNameValuePair("question.asker_email", ""));
+		formparams.add(new BasicNameValuePair("question.content", "Who are you?"));
+		formparams.add(new BasicNameValuePair("question.ip", "111.202.109.7"));
+		formparams.add(new BasicNameValuePair("question.is_private", "true"));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/add_question");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	//@Test
+	public void reply_question() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("question_id", "235"));
+		formparams.add(new BasicNameValuePair("reply_content", "who knows"));
+		formparams.add(new BasicNameValuePair("reply_user_id", ""));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/reply_question");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+	
+	@Test
+	public void reply_review() throws UnsupportedEncodingException, IOException, JSONException{
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		formparams.add(new BasicNameValuePair("sob.sob_code", "www.test.com"));
+		formparams.add(new BasicNameValuePair("sob.password", "111111"));
+		formparams.add(new BasicNameValuePair("sob.hotelgroup_id", "159"));
+		formparams.add(new BasicNameValuePair("review_id", "243"));
+		formparams.add(new BasicNameValuePair("reply_content", "who knows"));
+		formparams.add(new BasicNameValuePair("reply_user_id", ""));
+		
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+		HttpPost httppost = new HttpPost("http://test.chinapms.com:9001/gateway/reply_review");
+		httppost.setEntity(entity);
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpResponse httpResponse = client.execute(httppost); 
+		assertEquals(200, httpResponse.getStatusLine().getStatusCode());
+		HttpEntity result = httpResponse.getEntity(); 
+		String strResult = EntityUtils.toString(result); 
+		System.out.println(strResult);
+		JSONObject json = new JSONObject(strResult);
+		assertFalse(json.has("exception_code"));
+	}
+}
